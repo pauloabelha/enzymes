bd7c46b9a94896bd8e3a7217b0d0172bd0b18834
Paulo <pauloabelha@gmail.com>
2017-05-08 17:56:04 +0100

    Changes to ptool generation

--- ptools/GeneratePToolsForSimulation.m
+++ ptools/GeneratePToolsForSimulation.m
@@ -1,5 +1,8 @@
 % create one simulation-ready folder for each ptool for a given task
-function GeneratePToolsForSimulation( simulation_folder, ptools, task, P_orig, ptool_maps )  
+function GeneratePToolsForSimulation( simulation_folder, ptools, task, completing ) 
+    if ~exist('completing','var')
+        completing = 0;
+    end
     % flag for whether to write inertia to Gazebo folder
     WRITE_INERTIA = 0;
     % flag whether to run cluster decimation and simplify the pcl mesh
@@ -22,22 +25,28 @@ function GeneratePToolsForSimulation( simulation_folder, ptools, task, P_orig, p
     %% for each ptool: position, calculate inertia and write gazebo folder
 %     exist_pcl = exist('P','var');
     MIN_INERTIA = gazebo_params.MIN_INERTIA;
-    exist_pcl_orig = exist('P_orig','var');
     n_ptools = size(ptools,1);
-    parfor i=1:n_ptools
-        % rotate ptools for task
-        % rotate given pcl if it exists         
-%         if exist_pcl_orig            
-%             [SQ_grasp,SQ_action] = AlignPToolWithPCL( ptools(i,:), P_orig, ptool_maps(i,:) );            
-%             [ transf_list ] = GetTrasnfsSQsTask( SQ_grasp, SQ_action, task, SQ_grasp(end-2:end) );
-%             P = Apply3DTransfPCL(P_orig,transf_list);      
-%             SQs = {SQ_grasp,SQ_action};
-%             SQs = ApplyTransfSQs(SQs,transf_list);
-%         else
-            P = PTool2PCL(ptools(i,:),task);
-            [SQ_grasp,SQ_action] = GetPToolsSQs(ptools(i,:),task);   
-            SQs = {SQ_grasp,SQ_action};
-%         end       
+    if completing
+       ptool_folders = FindAllFolders( ['~/.gazebo/gazebo_models/' task '/' simulation_folder] ); 
+    end
+    for i=1:n_ptools        
+        if completing
+            ptool_already_processed = 0;
+            for j=1:numel(ptool_folders)
+                split_folder_name = strsplit(ptool_folders{j},'/');
+                if strcmp(split_folder_name{1}(end),num2str(i))
+                    ptool_already_processed = 1;
+                    break;
+                end
+            end
+            if ptool_already_processed
+                disp([simulation_folder ptool_folders{j} ': Ptool already processed']);
+                continue;
+            end
+        end
+        P = PTool2PCL(ptools(i,:),task);
+        [SQ_grasp,SQ_action] = GetPToolsSQs(ptools(i,:),task);   
+        SQs = {SQ_grasp,SQ_action};
         ptools(i,:) = CheckPToolMass(ptools(i,:));
         % position ptools for the task
         [ task_params, positioning_function ] = TaskPositioningParams( task );
--- superquadrics/CheckSQParams.m
+++ superquadrics/CheckSQParams.m
@@ -5,6 +5,7 @@ function [ SQ_ok ] = CheckSQParams( SQ )
     for i=1:3
        if SQ(i) <= 0.0001 
            SQ_ok = 0;
+           disp(SQ);
            error([EROR_SQ_WRONG_VALUE_RANGE num2str(i) ' (scale): ' num2str(SQ(i))]);
        end
     end
@@ -12,6 +13,7 @@ function [ SQ_ok ] = CheckSQParams( SQ )
     if size(SQ,2) == 16
         if SQ(4) < 0
             SQ_ok = 0;
+            disp(SQ);
             error([EROR_SQ_WRONG_VALUE_RANGE num2str(i) ' (toroid thickness): ' num2str(SQ(i))]);
         end
         shape_ix_beg=5; shape_ix_end=6;
@@ -27,27 +29,32 @@ function [ SQ_ok ] = CheckSQParams( SQ )
     for i=shape_ix_beg:shape_ix_end
         if SQ(i) < 0.009 || SQ(i) > 2
             SQ_ok = 0;
+            disp(SQ);
             error([EROR_SQ_WRONG_VALUE_RANGE num2str(i) ' (shape): ' num2str(SQ(i))]);
         end
     end
     for i=orient_ix_beg:orient_ix_end
         if SQ(i) < -3.2 || SQ(i) > 3.2
             SQ_ok = 0;
+            disp(SQ);
             error([EROR_SQ_WRONG_VALUE_RANGE num2str(i) ' (orientation): ' num2str(SQ(i))]);
         end
     end
     for i=taper_ix_beg:taper_ix_end
         if SQ(i) < -1 || SQ(i) > 1
             SQ_ok = 0;
+            disp(SQ);
             error([EROR_SQ_WRONG_VALUE_RANGE num2str(i) ' (tapering): ' num2str(SQ(i))]);
         end
     end
     if SQ(bend_alpha_ix) < 0 || SQ(bend_alpha_ix) > 100
         SQ_ok = 0;
+        disp(SQ);
         error([EROR_SQ_WRONG_VALUE_RANGE num2str(11) ' (bending): ' num2str(SQ(bend_alpha_ix))]);
     end
     if SQ(bend_k_ix) > 0 && SQ(bend_k_ix) < SQ(3)
         SQ_ok = 0;
+        disp(SQ);
         error([EROR_SQ_WRONG_VALUE_RANGE num2str(12) ' (bending): ' num2str(SQ(bend_k_ix))]);
     end
 end
