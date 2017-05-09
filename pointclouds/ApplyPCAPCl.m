3d4422ebbd39f6587c7179bcac0ce0ed08a7f2f5
Paulo <pauloabelha@gmail.com>
2017-05-09 12:11:00 +0100

    ApplyPCAPCL now centralises

--- pointclouds/ApplyPCAPCl.m
+++ pointclouds/ApplyPCAPCl.m
@@ -3,11 +3,13 @@ function [ P, pcl_pca ] = ApplyPCAPCl( P, pcl_pca )
     if ~exist('pcl_pca','var')
         pcl_pca = pca(P.v);
     end
+    P.v = P.v - mean(P.v);
     P.v = P.v*pcl_pca;
     if ~isempty(P.n)
         P.n = P.n*pcl_pca;
     end
     for i=1:size(P.segms,2)
+        P.segms{i}.v = P.segms{i}.v - mean(P.segms{i}.v);
         P.segms{i}.v = P.segms{i}.v*pcl_pca;
         if ~isempty(P.segms{i}.n)
             P.segms{i}.n = P.segms{i}.n*pcl_pca;
