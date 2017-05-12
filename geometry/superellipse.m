35250af92dc1371eb41366a367469c093df468f5
Paulo <pauloabelha@gmail.com>
2017-05-12 12:15:50 +0100

    Fixed sampling for superparabola

--- geometry/superellipse.m
+++ geometry/superellipse.m
@@ -1,6 +1,9 @@
 % By Paulo Abelha
 % returns or plots a superellipse
-function [ pcl, thetas ] = superellipse( a, b, eps1, plot_fig, not_unif )
+function [ pcl, thetas ] = superellipse( a, b, eps1, D, plot_fig, not_unif )
+    if ~exist('D','var')
+        D = 0.0005;
+    end
     if ~exist('plot_fig','var')
         plot_fig = 0;
     end
@@ -13,7 +16,7 @@ function [ pcl, thetas ] = superellipse( a, b, eps1, plot_fig, not_unif )
         X = a*signedpow(cos(thetas),eps1);
         Y = b*signedpow(sin(thetas),eps1);
     else
-        thetas = unif_sample_theta(a, b, eps1, N);
+        thetas = unif_sample_theta(a, b, eps1, D);
         X = zeros(1,size(thetas,2)*4);
         Y = X;
         ix = 1;
@@ -33,23 +36,24 @@ function [ pcl, thetas ] = superellipse( a, b, eps1, plot_fig, not_unif )
     end
 end
 
-function thetas = unif_sample_theta(a, b, eps1, N)
-    max_iter = 10^6;
-    D = 0.002;
-    thetas = zeros(1,N);
+function thetas = unif_sample_theta(a, b, eps1, D)
+    max_iter = 1e6;
     theta_increasing = 1;
+    thetas = 0;
     ix_eta = 1;
-    while (thetas(ix_eta) > -eps(1))
+    ns = [0 0 0];
+    while (thetas(end) > -eps(1))
         if ix_eta > max_iter
             error(['Angle eta sampling reach the maximum number of iterations ' num2str(max_iter)]);
         end
         % if theta got close enough to pi/2 start decreasing it down to 0        
-        if (thetas(ix_eta)  > pi/2 - eps(1))
-          thetas(ix_eta)  = max(pi/2 - thetas(ix_eta) , 0);
+        if thetas(end)  > pi/2 - 10*eps
+          thetas(end)  = max(pi/2 - thetas(end) , 0);
           theta_increasing = -1;
         end             
         % update angle
-        thetas(ix_eta+1) = thetas(ix_eta) + theta_increasing*update_theta(a, b, eps1, thetas(ix_eta), D);    
+        [theta_next, ns] = update_theta(a, b, eps1, thetas(end), D, ns);
+        thetas(end+1) = thetas(end) + theta_increasing*theta_next;    
         ix_eta = ix_eta +1;
     end   
 end
@@ -58,16 +62,19 @@ end
 % based on [PiluFisher95] Pilu, Maurizio, and Robert B. Fisher. �Equal-distance sampling of superellipse models.� DAI RESEARCH PAPER (1995)
 % 
 % Update theta based on the combinations of models in the paper
-function [ R ] = update_theta( a, b, epsilon, theta, D )
+function [ R, ns ] = update_theta( a, b, epsilon, theta, D, ns )
     theta_eps = 1e-1;
     if theta <= theta_eps
         % equation (8)
         R = power(abs((D/b)+power(theta,epsilon)),1/epsilon)-theta;
+        ns(1) = ns(1) + 1;
     else
         if (pi/2 - theta_eps) <= theta || theta >= (pi/2 + theta_eps)
             % equation (9)
-            R = power((D/a)+power(pi/2-theta,epsilon),1/epsilon)-(pi/2-theta);            
+            R = power((D/a)+power(pi/2-theta,epsilon),1/epsilon)-(pi/2-theta);      
+            ns(2) = ns(2) + 1;
         else
+            ns(3) = ns(3) + 1;
             % equation (5)
             %R = D_ * 1/((a^2+b^2)*abs(epsilon)*sqrt(sin(theta)^(2*epsilon-2))*abs(cos(theta)));
             R = (D / epsilon) * cos (theta) * sin (theta) * sqrt (1/ (a^2  * power(cos(theta), 2*epsilon) * power(sin(theta), 4.) + b^2 * power(sin(theta), 2*epsilon) * power(cos(theta), 4.)));
--- geometry/superparabola.m
+++ geometry/superparabola.m
@@ -1,6 +1,9 @@
 % By Paulo Abelha
 % returns or plots a superellipse
-function [ pcl, us ] = superparabola( a, b, eps1, plot_fig, not_unif )
+function [ pcl, us ] = superparabola( a, b, eps1, D, plot_fig, not_unif )
+    if ~exist('D','var')
+        D = 0.0005;
+    end
     if ~exist('plot_fig','var')
         plot_fig = 0;
     end
@@ -10,7 +13,7 @@ function [ pcl, us ] = superparabola( a, b, eps1, plot_fig, not_unif )
     N = 10^2;
     us = 0:1/N:1;    
     if ~not_unif
-        us = unif_sample_u(b, eps1, N);       
+        us = unif_sample_u(b, eps1, D);       
     end  
     X = a*us; 
     X = [X -X];
@@ -22,17 +25,15 @@ function [ pcl, us ] = superparabola( a, b, eps1, plot_fig, not_unif )
     end
 end
 
-function us = unif_sample_u(b, eps1, N)
+function us = unif_sample_u(b, eps1, D)
     max_iter = 10^6;
-    D = 0.001;
-    us = zeros(1,N);
     us(1) = 0;
     i = 1;
     while (us(i) < 1)
         if i > max_iter
             error(['Angle eta sampling reach the maximum number of iterations ' num2str(max_iter)]);
         end
-        us(i+1) = us(i) + update_u(b, eps1, us(i), D);    
+        us(end+1) = us(end) + update_u(b, eps1, us(end), D);    
         i = i +1;
     end   
 end
@@ -42,7 +43,7 @@ end
 % 
 % Update u based on the combinations of models in the paper
 function [ u_next ] = update_u( b, eps1, u_prev, D )
-    u_next = ((D*eps1)/(2*b)) * 1/sqrt( (4*b^2*(u_prev^((4/eps1) - 2)) ) + 1);
-%     u_next = ((D*eps1)/(2*b)) * 1/sqrt((u_prev^((2/eps1) - 1)) + 1);
+%     u_next = ((D*eps1)/(2*b)) * 1/sqrt( (4*b^2*(u_prev^((4/eps1) - 2)) ) + 1);
+    u_next = D / ( sqrt( ((4*b^2)/eps1^2)*u_prev^((4/eps1)-2) + 1 ) );
 end
 
