cd2975d6ecf204ba904f367206a21e21bbd11492
Paulo <pauloabelha@gmail.com>
2017-08-20 18:32:24 +0100

    Debugging

--- projection/ablation/ReadProjectionAblation.m
+++ projection/ablation/ReadProjectionAblation.m
@@ -1,12 +1,19 @@
-function [accs, metrics1] = ReadProjectionAblation(filepath, task)
+function [accs, accuracy_categs, metrics1] = ReadProjectionAblation(filepath, task)
     load(filepath);
     n_weights = size(weights,1);
     accs = zeros(n_weights,1);
     metrics1 = accs;
+    accuracy_categs = zeros(n_weights,4);
     for weight_ix=1:n_weights
         best_scores = best_scores_mtx(:,weight_ix);
         best_categ_scores = TaskCategorisation(best_scores,task);
         accs(weight_ix) = size(best_categ_scores(abs(best_categ_scores-tools_gt)==0),1)/size(best_categ_scores,1);
         metrics1(weight_ix) = Metric1(best_categ_scores',tools_gt',4);
+        % accuracy per categ        
+        for i=1:4
+            if size(best_categ_scores(tools_gt == i),1) ~= 0            
+                accuracy_categs(weight_ix,i) = size(best_categ_scores(best_categ_scores == i & tools_gt == i),1)/size(tools_gt(tools_gt == i),1);
+            end
+        end
     end
 end
--- projection/ablation/ReadProjectionAblations.m
+++ projection/ablation/ReadProjectionAblations.m
@@ -1,4 +1,4 @@
-function [ accs, metrics1, max_accs, max_metrics1 ] = ReadProjectionAblations( root_folder, task, file_date )
+function [ accs, accuracy_categs_all, metrics1, max_accs, max_metrics1 ] = ReadProjectionAblations( root_folder, task, file_date )
     %% check params
     if ~exist('root_folder','var')
         error('Please define a root folder as first param');
@@ -16,13 +16,18 @@ function [ accs, metrics1, max_accs, max_metrics1 ] = ReadProjectionAblations( r
     projection_file_prefix = ['projection_result_' task '_' file_date];
     projection_filenames = FindAllFilesOfPrefix(projection_file_prefix,root_folder);
     n_projs = numel(projection_filenames);
-    accs = ReadProjectionAblation([root_folder projection_filenames{1}]); 
-    accs = zeros(n_projs,size(accs,1));
+    accs = ReadProjectionAblation([root_folder projection_filenames{1}]);
+    n_weights = size(accs,1);
+    accs = zeros(n_projs,n_weights);
     metrics1 = accs;
-    n_seeds = zeros(n_projs,1);;
+    accuracy_categs_all = zeros(n_projs,n_weights,4);
+    n_seeds = zeros(n_projs,1);
     plot_legend_nseeds = cell(n_projs,1);
     for i=1:n_projs
-       [accs(i,:), metrics1(i,:)] = ReadProjectionAblation([root_folder projection_filenames{i}], task); 
+       [accs(i,:), accuracy_categs, metrics1(i,:)] = ReadProjectionAblation([root_folder projection_filenames{i}], task); 
+       accuracy_categs_all(i,:,:) = accuracy_categs;
+%        accs(i,:) = fliplr(accs(i,:));
+%        metrics1(i,:) = fliplr(metrics1(i,:));
        a=strsplit(projection_filenames{i},'_'); 
        n_seeds(i) = str2double(a{end-1});
        plot_legend_nseeds{i} = a{end-1};
@@ -31,11 +36,14 @@ function [ accs, metrics1, max_accs, max_metrics1 ] = ReadProjectionAblations( r
     max_metrics1 = max(metrics1,[],2);
     % plot accs 1
     figure; for i=1:size(accs,1) plot(accs(i,:)); hold on; end; hold off;
+    title('Accuracy accross weights');
     legend(plot_legend_nseeds);
     axes = gca;
     axes.YLim = [0 1];
+    % plot accs per categ    
     % plot metrics 1
     figure; for i=1:size(metrics1,1) plot(metrics1(i,:)); hold on; end;
+    title('Metric 1 accross weights');
     legend(plot_legend_nseeds);
     axes = gca;
     axes.YLim = [0 1];
