58a1c2bca055d1a578826a003b25bb268424b9fb
Lapin38 <benjamin.nougier@gmail.com>
2017-08-22 11:18:14 +0100

    IMproved GPR training function

--- projection/TrainRobot.m
+++ projection/TrainRobot.m
@@ -1,4 +1,4 @@
-function [ gprs, sigmaMs, feat_imps, dims_ixs ] = TrainRobot( ptools, scores, root_folder, task ) 
+function [ gprs, sigmaMs, feat_imps, dims_ixs, train_res ] = TrainRobot( ptools, scores, root_folder, task ) 
     if ~exist('task','var') || ~exist('root_folder','var')
         warning('No task or root folder defined. File is going to be save at home with task unkown');
         root_folder = '~/';
@@ -15,7 +15,7 @@ function [ gprs, sigmaMs, feat_imps, dims_ixs ] = TrainRobot( ptools, scores, ro
     while curr_n_dims < prev_n_dims 
         disp([curr_n_dims curr_n_dims]);
         prev_n_dims = curr_n_dims;
-        [ gprs{end+1}, train_res{end+1}.loss_regression, train_res{end+1}.mean_error, train_res{end+1}.ixs_train, train_res{end+1}.ixs_test ] = FitGPR( ptools(:,dims_ixs{end}), scores, 0.05, 1);        
+        [ gprs{end+1}, train_res{end+1}.loss_regression, train_res{end+1}.mean_train_error, train_res{end+1}.mean_test_error, train_res{end+1}.ixs_train, train_res{end+1}.ixs_test ] = FitGPR( ptools(:,dims_ixs{end}), scores, 0.25, 1);        
         [ sigmaMs{end+1}, feat_imps{end+1}, last_dim_ixs ] = PLotGPRLengthScales( gprs{end}, ptools(:,dims_ixs{end}), 0 );        
         dims_ixs{end+1} = dims_ixs{end};
         dims_ixs{end}(dims_ixs{end}==1) = last_dim_ixs;
