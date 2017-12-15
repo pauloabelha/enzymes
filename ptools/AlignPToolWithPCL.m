7120b9b56600b0e9789da187553d0e20b57aa060
Paulo Abelha <pauloabelha@gmail.com>
2017-12-15 21:06:06 -0200

    New things for function for tool info

--- ptools/AlignPToolWithPCL.m
+++ ptools/AlignPToolWithPCL.m
@@ -24,7 +24,7 @@ function [ SQ_grasp, SQ_action] = AlignPToolWithPCL( ptool, P, ptool_map, plot_f
     new_vec_centres = rot_SQ*vec_centres;    
     SQ_action(end-2:end) = SQ_grasp(end-2:end) + new_vec_centres';
     if plot_fig
-        PlotPCLSegments(P,-1,5000,{'.b'});
+        PlotPCLSegments(P,-1,5000,{'.b', '.k'});
         hold on;
         PlotSQs({SQ_grasp,SQ_action},10000);
     end    
--- ptools/ExtractPTools.m
+++ ptools/ExtractPTools.m
@@ -1,4 +1,4 @@
-function [ptools] = ExtractPTools(SQs, mass)
+function [ptools, ptool_maps] = ExtractPTools(SQs, mass)
     v=1:numel(SQs);
     alt_ixs = nchoosek(v,2);
     temp = zeros(size(alt_ixs,1)*2,2)+1;
@@ -13,14 +13,25 @@ function [ptools] = ExtractPTools(SQs, mass)
     alt_ixs = temp;
     max_n_alt = list_depth( SQs );
     ptools = [];
-    for i=1:max_n_alt
+    ptool_maps = [];
+    if max_n_alt == 1
         for j=1:size(alt_ixs,1)
-            if numel(SQs{alt_ixs(j,1)}) < i || numel(SQs{alt_ixs(j,2)}) < i
-                continue;
-            end
-            SQ_grasp = SQs{alt_ixs(j,1)}{i};
-            SQ_action = SQs{alt_ixs(j,2)}{i};
-            ptools(end+1,:) = ExtractPTool(SQ_grasp,SQ_action,mass);            
+            SQ_grasp = SQs{alt_ixs(j,1)};
+            SQ_action = SQs{alt_ixs(j,2)};   
+            [ptools(end+1,:), orig_grasp_centre, orig_vec_grasp] = ExtractPTool(SQ_grasp,SQ_action,mass); 
+            ptool_maps(end+1,:) = [orig_grasp_centre orig_vec_grasp'];
         end
-    end
+    else
+        for i=1:max_n_alt
+            for j=1:size(alt_ixs,1)
+                if numel(SQs{alt_ixs(j,1)}) < i || numel(SQs{alt_ixs(j,2)}) < i
+                    continue;
+                end
+                SQ_grasp = SQs{alt_ixs(j,1)}{i};
+                SQ_action = SQs{alt_ixs(j,2)}{i};    
+                [ptools(end+1,:), orig_grasp_centre, orig_vec_grasp] = ExtractPTool(SQ_grasp,SQ_action,mass); 
+                ptool_maps(end+1,:) = [orig_grasp_centre orig_vec_grasp'];
+            end
+        end
+    end    
 end
